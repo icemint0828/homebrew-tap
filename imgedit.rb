a7e34c4c2453d35d7a0f79ec479fe1db64e2ebd4
icemint0828 <icemint0828@gmail.com>
2022-06-25 10:42:30 +0900

    change to specify direct execution binaries

--- imgedit.rb
+++ imgedit.rb
@@ -2,17 +2,13 @@ HOMEBREW_CLI_VERSION='1.3.0'
 class Imgedit < Formula
   desc ""
   homepage "https://github.com/icemint0828/imgedit"
-  url "https://github.com/icemint0828/imgedit/archive/refs/tags/v#{HOMEBREW_CLI_VERSION}.tar.gz"
-  sha256 "75d885b22f1c3c3a01cee33b9fd3fa3e39a4aa441fcd5c894adef5b5c23064f5"
+  url "https://github.com/icemint0828/imgedit/releases/download/v#{HOMEBREW_CLI_VERSION}/imgedit_MacOS.zip"
+  sha256 "944e86dde914c09f7e92572aeb8373c15dacb655f3e1f3f76e8f6ec74c39ac86"
   license "MIT"
   version "v#{HOMEBREW_CLI_VERSION}"
 
-  # depends_on "cmake" => :build
-  depends_on "go" => :build
-
   def install
     cd buildpath do
-      system "go", "build", "-o", "imgedit", "./cmd/main.go" 
       bin.install "imgedit"
     end
   end
