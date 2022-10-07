952253e36f99e7233720fd8c546c2b5e7b298d0e
icemint0828 <icemint0828@gmail.com>
2022-10-07 17:46:56 +0900

    release v1.6.0

--- imgedit.rb
+++ imgedit.rb
@@ -1,9 +1,9 @@
-HOMEBREW_CLI_VERSION='1.5.0'
+HOMEBREW_CLI_VERSION='1.6.0'
 class Imgedit < Formula
   desc "imgedit is a package that performs image processing such as resizing and trimming."
   homepage "https://github.com/icemint0828/imgedit"
   url "https://github.com/icemint0828/imgedit/releases/download/v#{HOMEBREW_CLI_VERSION}/imgedit_MacOS.zip"
-  sha256 "d4868593f3412048e87c2b091e461ea34103e0fdb34be9c9180ca17a8fe9ffae"
+  sha256 "305f6f9f67e59163667dabff32e035d5267a63ddd7f2b88f8ad3d13a041fb316"
   license "MIT"
   version "v#{HOMEBREW_CLI_VERSION}"
 
